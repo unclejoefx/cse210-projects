7d520206af969837c9360b901bac5145c3a8ae83
Uwa Joseph Uwa <uwauwajoe@gmail.com>
2024-01-15 04:24:25 +0100

    Prep 2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+        if (percent >= 93)
+        {
+            letter = "A+";
+        }
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }   
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't pass the class, better luck next time!");
+        }
     }
 }
